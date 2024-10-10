90cdeca70cd88684146aa62584505d4250061ed3
47thommy <47thommy@gmail.com>
2024-10-10 19:00:30 +0300

    feat: using slot to wrap other components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 	import NavBar from "@/components/NavBar.vue";
 	import Hero from "@/components/Hero.vue";
+	import HomeCard from "@/components/HomeCard.vue";
 </script>
 <template>
 	<NavBar />
 	<Hero />
+	<HomeCard />
 </template>
