5182cf475b432a96568378120431fba363ea6f14
47thommy <47thommy@gmail.com>
2024-10-10 22:57:57 +0300

    feat: learned computed properties by adding job listings

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 	import NavBar from "@/components/NavBar.vue";
 	import Hero from "@/components/Hero.vue";
 	import HomeCard from "@/components/HomeCard.vue";
+	import JobListings from "@/components/JobListings.vue";
 </script>
 <template>
 	<NavBar />
 	<Hero />
 	<HomeCard />
+	<JobListings :limit="3" :showButton="true" />
 </template>
