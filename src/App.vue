44d94916189183f5b0118d8a7f4fc4b51e63a927
47thommy <47thommy@gmail.com>
2024-10-10 14:42:48 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+	export default {
+		//  this is using options api
+		// data() {
+		// 	return {
+		// 		name: "Thomas Wondwosen",
+		// 		status: "inactive",
+		// 		tasks: ["task one", "task two", "task three", "task four", "task five"],
+		// 		link: "https://google.com",
+		// 	};
+		// },
+		// methods: {
+		// 	changeStatus() {
+		// 		if (this.status === "active") {
+		// 			this.status = "pending";
+		// 		} else if (this.status === "pending") {
+		// 			this.status = "inactive";
+		// 		} else {
+		// 			this.status = "active";
+		// 		}
+		// 	},
+		// },
+	};
+</script>
+
+<template>
+	<h1>{{ name }}</h1>
+	<p v-if="status === 'active'">User account is active</p>
+	<p v-else-if="status === 'pending'">User account is in pending status</p>
+	<p v-else>User account is InActive</p>
+
+	<ul>
+		Tasks:
+		<li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+	</ul>
+	<a :href="link">Google</a>
+	<br />
+	<button @click="changeStatus">Change Status</button>
+</template>
